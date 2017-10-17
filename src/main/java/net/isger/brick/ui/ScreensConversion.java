42e03395cea6c50a1c4011de78cb38c4cf1e1f49
issing <issing@isger.net>
2017-10-17 14:22:21 +0800

    .

--- src/main/java/net/isger/brick/ui/ScreensConversion.java
+++ src/main/java/net/isger/brick/ui/ScreensConversion.java
@@ -1,8 +1,10 @@
 package net.isger.brick.ui;
 
 import java.io.File;
+import java.lang.reflect.Type;
 
 import net.isger.brick.util.ScanLoader;
+import net.isger.util.Reflects;
 import net.isger.util.Strings;
 import net.isger.util.reflect.conversion.Conversion;
 import net.isger.util.scan.ScanFilter;
@@ -36,11 +38,11 @@ public class ScreensConversion extends ScanLoader implements Conversion {
         return INSTANCE;
     }
 
-    public boolean isSupport(Class<?> type) {
-        return Screens.class.isAssignableFrom(type);
+    public boolean isSupport(Type type) {
+        return Screens.class.isAssignableFrom(Reflects.getRawClass(type));
     }
 
-    public Object convert(Class<?> type, Object res) {
+    public Object convert(Type type, Object res) {
         return new Screens(toList(load(res)));
     }
 
